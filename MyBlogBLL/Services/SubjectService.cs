545a9a7eec5b16c36e183cf5751f8a3b1ad6b3a6
tarikcelebi <tarikcelebi97@gmail.com>
2024-01-27 20:39:03 +0300

    BLL Services created.

---
+++ MyBlogBLL/Services/SubjectService.cs
@@ -0,0 +1,56 @@
+﻿using MyBlogDAL.Repositories.Abstract;
+using MyBlogDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogBLL.Services
+{
+    public class SubjectService
+    {
+        private readonly ISubjectRepository subjectRepository;
+
+        public SubjectService(ISubjectRepository subjectRepository)
+        {
+            this.subjectRepository = subjectRepository;
+        }
+
+        IEnumerable<Subject> GetAllSubjects()
+        {
+            return subjectRepository.GetAll();
+        }
+
+        bool AddSubject(Subject subject)
+        {
+            return subjectRepository.Add(subject);
+
+        }
+
+        bool UpdateSubject(Subject subject)
+        {
+            return subjectRepository.Update(subject);
+        }
+
+        bool DeleteSubject(Subject subject)
+        {
+            return subjectRepository.Delete(subject);
+        }
+
+        Subject GetSubject(int id)
+        {
+            return subjectRepository.GetById(id);
+        }
+
+
+
+
+
+
+
+
+
+        
+    }
+}
